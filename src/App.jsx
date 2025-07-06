95a6e3ac8102db675faca22f51b8854412fc4ee0
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-07-07 01:32:22 +0530

    heroSection animations added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import NavBar from './components/NavBar'
-
+import HeroSection from './section/HeroSection'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger)
 const App = () => {
   return (
-    <div className=' '>
-      <NavBar/>
-    </div>
+    <main>
+            <NavBar/>
+      <HeroSection/>
+      <div className='h-dvh border-2 bg-yellow-400'></div>
+    </main>
   )
 }
 
