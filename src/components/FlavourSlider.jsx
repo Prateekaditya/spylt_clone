2dd1df339d4097434cb2ad2e0f6ee634c4e0a84e
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-07-07 21:47:49 +0530

    added images in flavor

--- src/components/FlavourSlider.jsx
+++ src/components/FlavourSlider.jsx
@@ -1,8 +1,20 @@
+import { flavorlists } from "../constants"
 
 const FlavourSlider = () => {
   return (
     <div className="slider-wrapper">
-      <div className="flavors"></div>
+      <div className="flavors">
+        {
+          flavorlists.map((flavor)=>(
+            <div key={flavor.name} className="relative z-30 lg:w-[50vw] w-96 flex-none lg:h-[70vh] md:h-[50vh] md:w-[90vw] h-80">
+              <img src={`/images/${flavor.color}-bg.svg`} alt={`${flavor.color}`} className="absolute bottom-0"  />
+              <img src={`/images/${flavor.color}-drink.webp`} alt={`${flavor.color}`} className="drinks"  />
+              <img src={`/images/${flavor.color}-elements.webp`} alt={`${flavor.color}`} className="elements"  />
+            </div>
+          )
+          )
+        }
+      </div>
     </div>
   )
 }
