ecc8bb3c2eae64dbc2a99331d9fd6a0897eb245d
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-07-07 21:29:41 +0530

    adding flavor section editing flavor section

---
+++ src/components/FlavourSlider.jsx
@@ -0,0 +1,10 @@
+
+const FlavourSlider = () => {
+  return (
+    <div className="slider-wrapper">
+      <div className="flavors"></div>
+    </div>
+  )
+}
+
+export default FlavourSlider
--- src/section/Flavour.jsx
+++ src/section/Flavour.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import FlavourTitle from '../components/FlavourTitle'
+import FlavourSlider from '../components/FlavourSlider'
 
 const Flavour = () => {
   return (
-    <div>Flavour</div>
+    <div className='flavor-section'>
+      <div className="h-full flex lg:flex-row flex-col items-center relative">
+        <div className="lg:w-[57%] flex-none h-80 lg:h-full md:mt-20 xl:mt-0">
+            <FlavourTitle/>
+        </div>
+        <div className="h-full">
+            <FlavourSlider/>
+        </div>
+      </div>
+    </div>
   )
 }
 
